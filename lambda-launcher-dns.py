ae652951a841c5ddf66062839f63cd366839c6fc
Michael Ludvig <mludvig@logix.net.nz>
2017-08-07 12:35:31 +1200

    Added import-files.py for %%{filename.py}%% importing
    Useful for embedding Lambda functions into the templates.

---
+++ lambda-launcher-dns.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+# AWS Lambda function that (re)associates the provided
+# Elastic IP to a newly started instance in an Auto Scaling Group
+# The ASG must be configured with min=1/max=1 obviously.
+
+# The EIP must be provided in the function environment, e.g.
+# elastic_ip = 54.12.34.56
+
+# By Michael Ludvig - https://aws.nz
+
+import os
+import json
+import boto3
+
+ec2 = boto3.client('ec2')
+route53 = boto3.client('route53')
+
+def assign_eip(instance, elastic_ip):
+    print('%s - Associate EIP %s' % (instance['InstanceId'], elastic_ip))
+
+    old_public_ip = instance['PublicIpAddress']
+
+    if old_public_ip == elastic_ip:
+        print('%s - Instance already has EIP %s' % (instance['InstanceId'], elastic_ip))
+        return
+
+    response = ec2.associate_address(InstanceId=instance['InstanceId'], PublicIp=elastic_ip, AllowReassociation=True)
+    if not response:
+        print('Elastic IP %s not found' % elastic_ip)
+        raise
+
+    try:
+        instances = ec2.describe_instances(InstanceIds=[instance['InstanceId']])
+        instance = instances['Reservations'][0]['Instances'][0]
+    except IndexError, e:
+        print('%s - Instance not found' % instance['InstanceId'])
+        raise
+
+    new_public_ip = instance['PublicIpAddress']
+    if new_public_ip != elastic_ip:
+        print('%s - Association of EIP %s failed (currrent Public IP: %s)' %
+                (instance['InstanceId'], elastic_ip, new_public_ip))
+        raise
+
+    print('%s - Elastic IP %s assigned, old IP was %s' % (instance['InstanceId'], new_public_ip, old_public_ip))
+    return
+
+def update_dns(instance, hosted_zone_id, dns_name):
+    print('%s - Updating %s to %s' % (instance['InstanceId'], dns_name, instance['PrivateIpAddress']))
+
+    response = route53.change_resource_record_sets(
+        HostedZoneId = hosted_zone_id,
+        ChangeBatch = {
+            'Comment': 'Update for instance %s / %s' % (instance['InstanceId'], instance['PrivateIpAddress']),
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': dns_name,
+                        'Type': 'A',
+                        'TTL': 60,
+                        'ResourceRecords': [ { 'Value': instance['PrivateIpAddress'] } ]
+                    }
+                }
+            ]
+        }
+    )
+    if not response:
+        print('%s - Update for %s to %s failed' % (instance['InstanceId'], dns_name, instance['PrivateIpAddress']))
+        raise
+    print('%s - Updated %s to %s' % (instance['InstanceId'], dns_name, instance['PrivateIpAddress']))
+    return
+
+def lambda_handler(event, context):
+    try:
+        elastic_ip = os.environ['elastic_ip']
+        hosted_zone_id = os.environ['hosted_zone_id']
+        dns_name = os.environ['dns_name']
+    except:
+        print('Environment variables [elastic_ip, hosted_zone_id, dns_name] must be set')
+        raise
+
+    for record in event['Records']:
+        message = record['Sns']['Message']
+        j = json.loads(message)
+        if j['Event'] != 'autoscaling:EC2_INSTANCE_LAUNCH':
+            print('Ignoring event: ' + j['Event'])
+            continue
+        instance_id = j['EC2InstanceId']
+        try:
+            instances = ec2.describe_instances(InstanceIds=[instance_id])
+            instance = instances['Reservations'][0]['Instances'][0]
+        except IndexError, e:
+            print('%s - Instance not found' % instance_id)
+            raise
+        print('%s - Processing launch actions' % (instance_id))
+        assign_eip(instance, elastic_ip)
+        update_dns(instance, hosted_zone_id, dns_name)
---
+++ lambda-snapshot-asg.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# AWS Lambda function for creating an image from a given instance.
+# By Michael Ludvig - https://aws.nz
+
+# Trigger this function from CloudWatch Scheduler (cron-like)
+# Pass the Instance ID in 'instance_id' environment variable.
+
+from __future__ import print_function
+import os
+import boto3
+from datetime import datetime, timedelta
+import time
+
+ec2 = boto3.client('ec2')
+cfn = boto3.client('cloudformation')
+asc = boto3.client('autoscaling')
+
+def find_asg_instances(asg_name):
+    ids = []
+    try:
+        r = asc.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
+        asg = r['AutoScalingGroups'][0]
+        for i in asg['Instances']:
+            if i['LifecycleState'] == 'InService':
+                ids.append(i['InstanceId'])
+        return ids
+    except:
+        print('%s - Unable to list InService instances' % asg_name)
+        raise
+
+def create_image(instance_id):
+    def _print_log(message):
+        print('%s @ %s: %s' % (instance_id, snapshot_timestamp, message))
+
+    snapshot_timestamp = datetime.strftime(datetime.now(), '%s')
+    _print_log('Snapshotting instance')
+    instance = ec2.describe_instances(InstanceIds=[instance_id])
+    description = ''
+    tags = {}
+
+    try:
+        tags = {item['Key']:item['Value'] for item in instance['Reservations'][0]['Instances'][0]['Tags']}
+    except:
+        pass
+
+    if 'Name' in tags:
+        description = tags['Name']
+    elif 'aws:cloudformation:stack-name' in tags:
+        description = tags['aws:cloudformation:stack-name']
+    else:
+        description = instance_id
+
+    name = instance_id + '_' + snapshot_timestamp
+    description = description + ' ' + datetime.strftime(datetime.now(), '%Y-%m-%d %H-%M-%S')
+    r = ec2.create_image(InstanceId = instance_id, Name = name, Description = description, NoReboot = False)
+    image_id = r['ImageId']
+    _print_log('Created image: id=%s name=%s' % (image_id, name))
+    image_tags = [
+        {'Key': 'SnapshotTimestamp', 'Value': snapshot_timestamp },
+        {'Key': 'InstanceId', 'Value': instance_id }
+    ]
+    if 'Name' in tags:
+        image_tags.append({ 'Key': 'Name', 'Value': tags['Name'] })
+    ec2.create_tags(Resources = [image_id], Tags = image_tags)
+    image_tags_string = ' '.join(map(lambda x: '%(Key)s=%(Value)s' % x, image_tags))
+    _print_log('Created tags: %s' % (image_tags_string))
+
+    return (image_id, snapshot_timestamp)
+
+def update_cfn_stack(stack_name, image_id_param, image_id):
+    stack = cfn.describe_stacks(StackName = stack_name)
+    params_new = []
+    for param in stack['Stacks'][0]['Parameters']:
+        if param['ParameterKey'] != image_id_param:
+            params_new.append(param)
+        else:
+            params_new.append({'ParameterKey': image_id_param, 'ParameterValue': image_id})
+    cfn.update_stack(StackName = stack_name, UsePreviousTemplate = True, Parameters = params_new, Capabilities=['CAPABILITY_IAM'])
+    print('%s - Updated parameter %s to %s' % (stack_name, image_id_param, image_id))
+
+def deregister_old_images(instance_id, retain_days):
+    oldest_time = datetime.now() - timedelta(days = retain_days)
+    oldest_timestamp = int(time.mktime(oldest_time.timetuple()))
+    print('Purging images older than: %s' % oldest_time.strftime('%Y-%m-%d %H-%M-%S'))
+
+    images = ec2.describe_images(Owners=['self'], Filters=[
+        { 'Name': 'tag:InstanceId', 'Values': [ instance_id ] },
+        { 'Name': 'tag-key', 'Values': [ 'SnapshotTimestamp' ] }
+    ])
+    for image in images['Images']:
+        try:
+            tags = {item['Key']:item['Value'] for item in image['Tags']}
+            snapshot_timestamp = int(tags['SnapshotTimestamp'])
+        except:
+            continue
+        if snapshot_timestamp < oldest_timestamp:
+            print('%s: Deregistering image' % image['ImageId'])
+            ec2.deregister_image(ImageId = image['ImageId'])
+        else:
+            print('%s: Retaining image: name=%s created=%s' % (image['ImageId'], image['Name'], image['CreationDate']))
+
+def lambda_handler(event, context):
+    try:
+        asg_name = os.environ['asg_name']
+        cfn_stack_name = os.environ['cfn_stack_name']
+        cfn_ami_parameter = os.environ['cfn_ami_parameter']
+        retain_days = int(os.environ['retain_days'])
+    except:
+        print('Environment variables [asg_name, cfn_stack, cfn_ami_parameter, retain_days] must be set')
+        raise
+
+    ids = find_asg_instances(asg_name)
+    if len(ids) < 1:
+        print('%s - No instances InService found' % asg_name)
+        raise
+    if len(ids) > 1:
+        print('%s - Too many InService instances in ASG. This only works with min=max=1 ASGs!' % asg_name)
+        raise
+
+    instance_id = ids[0]
+
+    image_id, snapshot_timestamp = create_image(instance_id)
+    update_cfn_stack(cfn_stack_name, cfn_ami_parameter, image_id)
+    deregister_old_images(instance_id, retain_days)
+
+    return image_id
